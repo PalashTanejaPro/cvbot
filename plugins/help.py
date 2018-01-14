7ff6330306c39d3626e293a83841541d301f3da2
PalashTanejaPro <tanejapalash@gmail.com>
2018-01-14 20:34:46 +0530

    changed re pattern

--- plugins/help.py
+++ plugins/help.py
@@ -8,7 +8,7 @@ class help(BotPlugin):
     List the bot rules
     """
 
-    @re_botcmd(pattern=r'help.*',
+    @re_botcmd(pattern=r'help',
                re_cmd_name_help='help',
                flags=re.IGNORECASE,
                template='help.jinja2')
