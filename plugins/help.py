95d3382b2fc8a2fa5378e0d635c3fad0dc93639e
PalashTanejaPro <tanejapalash@gmail.com>
2018-01-14 20:46:50 +0530

    minor bug fix

--- plugins/help.py
+++ plugins/help.py
@@ -8,7 +8,7 @@ class help(BotPlugin):
     List the bot rules
     """
 
-    @re_botcmd(pattern=r'help',
+    @re_botcmd(pattern=r'commands',
                re_cmd_name_help='commands',
                flags=re.IGNORECASE,
                template='help.jinja2')
